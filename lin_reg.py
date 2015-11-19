dc87e0d336346c0eff699344489d4fee652c9588
RationalAsh <ashwinnarayan1994@gmail.com>
2015-11-19 19:20:56 +0530

    Made some changes

--- lin_reg.py
+++ lin_reg.py
@@ -20,8 +20,8 @@ def gradientf(X, y, theta):
 t = np.arange(0,10,0.01)
 y = 2*square(t) + 0*np.random.random(t.shape)
 
-X = np.array([[1, np.sin(x), np.sin(3*x), np.sin(5*x), np.sin(7*x)] for x in t])
-th = np.zeros(5)
+X = np.array([[1, np.sin(x), np.sin(3*x), np.sin(5*x), np.sin(7*x), np.sin(9*x)] for x in t])
+th = np.zeros(6)
 
 errors = []
 thetas = []
@@ -38,7 +38,7 @@ for i in xrange(numiters):
     th = th - alpha*grad
     errors.append(costf(X,y,th))
     thetas.append(th)
-    if(i%10 == 0):
+    if(i%20 == 0):
         print "Iteration: "+str(i)
         print "Costf: "+ str(costf(X,y,th))
         print "Gradient: " + str(gradientf(X, t, th))
@@ -50,12 +50,13 @@ th_opt = np.dot(np.linalg.pinv(X), y)
 y_opt = np.dot(X, th_opt)
 
 #Plotting results
-plt.plot(t, y, 'o')
+plt.plot(t, y)
+plt.ylim(-3,3)
 plt.xlabel('x')
 plt.ylabel('y')
 plt.hold(True)
-plt.plot(t, y_)
-plt.plot(t, y_opt)
+#plt.plot(t, y_)
+#plt.plot(t, y_opt)
 
 plt.figure()
 plt.plot(errors)
